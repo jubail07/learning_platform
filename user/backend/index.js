edfdbf0f3be615f956944a3c6c6a06e5b21d2718
Jubail <ibnujubail2003@gmail.com>
2025-11-10 11:31:54 +0530

    cors fix

--- user/backend/index.js
+++ user/backend/index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+app.use(cors({ origin: ['http://localhost:5173', 'https://learning-platform-1-0yd3.onrender.com'], credentials: true }))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
